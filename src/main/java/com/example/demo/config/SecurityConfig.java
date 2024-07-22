eb44215655e99cb28190ea5b554af464c5f8162e
RhesaDav (JetBrain) <rhesadav48@gmail.com>
2024-07-23 06:53:09 +0700

    update security

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,2 +1,30 @@
-package com.example.demo.config;public class SecurityConfig {
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(authorizeRequest->
+						authorizeRequest
+								.requestMatchers("/api/user/**").permitAll()
+								.requestMatchers("/api/expense/**").authenticated()
+								.anyRequest().denyAll()
+				).csrf(csrf -> csrf.disable());
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/main/java/com/example/demo/dto/ExpenseUpdateDTO.java
+++ src/main/java/com/example/demo/dto/ExpenseUpdateDTO.java
@@ -1,2 +1,50 @@
-package com.example.demo.dto;public class ExpenseUpdateDTO {
+package com.example.demo.dto;
+
+import com.example.demo.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+public class ExpenseUpdateDTO {
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @PastOrPresent(message = "Date must be in the past or present")
+    private Date date;
+
+    @NotNull(message = "Amount is required")
+    @Min(value = 1, message = "Amount must be at least 1")
+    private Double amount;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 }
